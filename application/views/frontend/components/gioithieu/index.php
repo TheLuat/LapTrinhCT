6979cede8aef0c58418988616f4246bfb276060a
Thetaul <luat2602@gmail.com>
2020-12-20 13:27:21 +0700

    project

---
+++ application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <h1>Trang giới thiệu</h1>
+    <h3>
+        Đây là project database của nhóm 9!
+    </h3>
+</div>
---
+++ application/views/frontend/modules/footer.php
@@ -0,0 +1,60 @@
+<footer id="footer">
+    <div class="news-social" style="background: #f9ca24; height: 50px">
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <!-- <ul class="list-unstyled social pull-right">
+                  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                  <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+              </ul> -->
+          </div>
+      </div> 
+  </div>
+  <div class="top-footer">
+    <div class="container">
+        <div class="col-xs-12 col-sm-12 col-md-6">
+            <div class="col-xs-12 col-sm-6">
+                <a href="trang-chu">
+                    <img src="public/images/B-watch.png" style="width: 80%; height: 80%" class="img-fix f-logo" alt="smart-construction">
+                </a>
+                <div class="about-store">
+                 Shop đồng hồ chất lượng- Giá cả hợp lý
+             </div>
+         </div>
+         <div class="col-xs-12 col-sm-6">
+            <div class="f-contact">
+                <h3>Thông tin liên hệ</h3>
+                <ul class="list-unstyled">
+                    <li class="clearfix">
+                        <i class="fa fa-map-marker"></i>
+                        <span>Số 1, Đại Cồ Việt, Hai Bà Trưng, Hà Nội</span>
+                    </li>
+                    <li class="clearfix">
+                        <i class="fa fa-phone"></i>
+                        <span>0358082646</span>
+                    </li>
+                    <li class="clearfix">
+                        <i class="fa fa-envelope"></i>
+                        <span><a href="mailto:dominhthong99@gmail.com">dominhthong99@gmail.com</a></span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-6">
+        <div class="col-xs-12 col-sm-6">
+            <h3 class="footer-title">HỖ TRỢ KHÁCH HÀNG</h3>
+            <ul class="list-unstyled linklists">
+                <li><a href="chinh-sach">Hướng dẫn thanh toán</a></li>          
+                <li><a href="chinh-sach">Hướng dẫn đặt hàng</a></li>
+                <li><a href="dieu-khoan">Điều khoản</a></li>                   
+            </ul>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+           <!-- <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=200&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" width="340" height="200" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe> -->
+       </div>
+   </div>
+</div>
+</div>
+</footer>
